350d63595f2900e13fc5ceb00bae46508fbd8ca5
IanZimmerK <ianzimmermann18@gmail.com>
2022-08-25 10:11:24 -0300

    Act6

---
+++ src/Actividades/API.java
@@ -0,0 +1,23 @@
+public class API {
+    public static void main(String[] args) {
+
+        System.out.println("El valor es: " +Math.sin(10));
+
+        System.out.println("El valor es: " +Math.cos(10));
+
+        System.out.println("El valor es: " +Math.tan(10));
+
+        System.out.println("El valor es: " +Math.atan(10));
+
+        System.out.println("El valor es: " +Math.atan2(10,20));
+
+        System.out.println("El valor es: "+Math.exp(10));
+
+        System.out.println("El valor es: " +Math.log(10));
+
+        System.out.println("El valor es: " +Math.PI);
+
+        System.out.println("El valor es: " +Math.E);
+
+    }
+}
