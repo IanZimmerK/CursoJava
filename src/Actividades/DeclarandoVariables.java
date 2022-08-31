c1c938ecf79c331402d5ff3901d48ab0e590ab46
IanZimmermann <ianzimmermann18@gmail.com>
2022-08-31 10:20:35 -0300

    Delete DeclarandoVariables.java

--- src/Actividades/DeclarandoVariables.java
+++
@@ -1,9 +0,0 @@
-public class DeclarandoVariables {
-    public static void main(String[] args) {
-        String nombre = "Ian";
-        int edad = 19;
-        double salario = 100000.50;
-        boolean carnet = true;
-
-    }
-}
