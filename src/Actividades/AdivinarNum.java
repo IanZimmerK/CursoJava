9309de0f43655bbb136e5fc288ca372b88a24ee8
IanZimmerK <ianzimmermann18@gmail.com>
2022-08-31 23:45:50 -0300

    Act10

---
+++ src/Actividades/AdivinarNum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AdivinarNum {
+    public static void main(String[] args) {
+        //variables a utilizar
+        Scanner scanner = new Scanner(System.in);
+        int num, intentos = 0;
+        int numeroaleatorio;
+        //genero el numero aleatorio entre 1-100
+        numeroaleatorio = (int) (Math.random() * 100 + 1);
+        System.out.println(numeroaleatorio);
+        do {
+            System.out.println("Digite un numero: ");
+            num = scanner.nextInt();
+            if (numeroaleatorio > num) {
+                System.out.println("El numero es mayor, intente nuevamente.. ");
+                intentos++;
+                System.out.println("Numero de intentos: "+intentos);
+            }
+            else {
+                System.out.println("El numero es menor, intente nuevamente..");
+                intentos++;
+                System.out.println("Numero de intentos: "+intentos);
+            }
+        }while (num!=numeroaleatorio);
+        System.out.println("Correcto!!");
+    }
+}
